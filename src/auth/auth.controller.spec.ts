4d1849f2bca5371122369ccc6381ba8b780a4769
Robert Weiße <rweisse@users.noreply.github.com>
2024-01-10 14:43:03 +0100

    Fixed specs of unit tests

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +9,7 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [AuthService, JwtService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/task/task.controller.spec.ts
+++ src/task/task.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TaskController } from './task.controller';
+import { TaskService } from './task.service';
 
 describe('TaskController', () => {
   let controller: TaskController;
@@ -7,6 +8,7 @@ describe('TaskController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TaskController],
+      providers: [TaskService],
     }).compile();
 
     controller = module.get<TaskController>(TaskController);
