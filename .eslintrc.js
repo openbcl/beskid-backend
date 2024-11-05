a505adcf66cabd895bc37eaa6bed71de5089fd02
Robert Weiße <rweisse@users.noreply.github.com>
2024-11-05 16:49:49 +0100

    eslintrc + prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,15 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'max-len': ['warn', {
+      'code': 160,
+      'ignoreComments': true,
+      'ignoreTrailingComments': true,
+      'ignoreUrls': true,
+      'ignoreStrings': true,
+      'ignoreTemplateLiterals': true,
+      'ignoreRegExpLiterals': true,
+    }],
   },
 };
